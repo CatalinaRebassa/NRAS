c2cb341c62aa576141e3cc40be1c8078a5092ca0
CatalinaRebassa <crebassa95@gmail.com>
2023-07-18 19:11:15 +0200

    hgf

--- Code/train.sh
+++
@@ -1,2 +0,0 @@
-#
-chemprop_train --data_path ../Data/Class_NRAS.csv --dataset_type classification --save_dir checkpoints --target_columns IC50 
--- Code/trainR80.sh
+++
@@ -1,1 +0,0 @@
-chemprop_train --data_path ../Data/R80_NRAS.csv --dataset_type classification --save_dir checkpoints --target_columns Random80 --features_generator rdkit_2d_normalized --no_features_scaling
